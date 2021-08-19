9efa916f6c1ead2268697f2a208f7c5cc9a3d315
Vicente Estrada <201112007@alumnos.utn.edu.mx>
2021-08-18 20:23:18 -0500

    Contructores

--- src/operaciones/Aritmetica.java
+++ src/operaciones/Aritmetica.java
@@ -4,6 +4,15 @@ public class Aritmetica {
     int a;
     int b;
 
+    //Constructor
+    public Aritmetica(){}
+
+    //Sobre carga de constructores
+    public Aritmetica( int arg1, int arg2 ){
+        this.a = arg1;
+        this.b = arg2;
+    }
+
     public void Sumar(){
         int result = a +b;
         System.out.println("El resultado es: " + result);
--- src/operaciones/Main.java
+++ src/operaciones/Main.java
@@ -5,12 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Aritmetica arit = new Aritmetica();
 
-        arit.a = 10;
-        arit.b = 5;
-        arit.Sumar();
-        int resultado = arit.SumarRetorno();
-        System.out.println(resultado);
+        Aritmetica art1 = new Aritmetica(1, 5);
 
-        System.out.println(arit.SumarConArgumentos(5,20));
+        System.out.println(art1.a);
+        System.out.println(art1.b);
     }
 }
